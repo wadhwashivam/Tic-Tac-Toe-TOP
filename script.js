8097e798a0f0b16bdd76758eaf03b59e780d62ed
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-02-09 18:55:39 -0400

    Event Delegation is done. Linked Markers to the players. Game is playable. Only thing left is resetting the board. And also will remove the board by default to show. It will only show once the start Game button is pressed.

--- script.js
+++ script.js
@@ -51,34 +51,49 @@ const gameController = (function (){
         textBox.appendChild(content);
     }
 
+    const resultBox = document.querySelector("#resultBox");
+    const content = document.createElement("h3");
     function playMove(row, column){
         if (gameOver){
+            content.innerText = "Game is already over."
+            resultBox.appendChild(content);
+
             console.log("Game is already over.");
             return;
         }
 
         if (board[row][column] !== undefined){
+            content.innerText = "Invalid move: Cell Already Occupied";
+            resultBox.appendChild(content);
+
             console.log("Invalid move: Cell Already Occupied");
             return;
         }
 
         board[row][column] = activePlayer.marker;
 
+        const placedMarker = activePlayer.marker;
         const result = checkWinner(row, column);
         if (result !== null){
             gameOver = true;
+            content.innerText = `${activePlayer.name} wins!`;
+            resultBox.appendChild(content);
+
             console.log(`${activePlayer.name} wins!`);
-            return;
+            return placedMarker;
         }
 
         if (checkTie()){
             gameOver = true;
+            content.innerText = "It's a tie!";
+            resultBox.appendChild(content);
+
             console.log("It's a tie!");
-            return;
+            return placedMarker;
         }
     
         switchPlayerTurn();
-
+        return placedMarker;
     }
 
     function checkRowWin(rowIndex){
@@ -98,7 +113,7 @@ const gameController = (function (){
     }
 
 
-    function checkDiagonalWin(rowIndex, colIn){
+    function checkDiagonalWin(){
         if (board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2]){
             return board[0][0];
         }
@@ -125,25 +140,43 @@ const gameController = (function (){
         return null;
     }
 
-function checkTie(){
-    for (let row = 0; row<board.length; row++){
-        for (let col = 0;col< board[row].length; col++){
-            if (board[row][col] === undefined){
-                return false;
+    function checkTie(){
+        for (let row = 0; row<board.length; row++){
+            for (let col = 0;col< board[row].length; col++){
+                if (board[row][col] === undefined){
+                    return false;
+                }
             }
         }
+        return true;
     }
-    return true;
-}
 
     return {startGame, playMove, getActivePlayer};
 })();
 
-function displayController(){
-    
+function xMarker(location) {
+    location.innerText = "X";
+    location.classList.add("cellDecoration");
 }
 
+function oMarker(location) {
+    location.innerText = "0";
+    location.classList.add("cellDecoration");
+}
+// Event Delegation
+document.addEventListener("click", (event) => {
+    if (event.target.matches(".cell")){
+        const cell = event.target;
+        const col = parseInt(cell.dataset.col);
+        const row = parseInt(cell.dataset.row);
 
+        const marker = gameController.playMove(row,col);
 
-// Form Handling
-
+        if (!marker) return;
+        if (marker === "X"){
+            xMarker(cell);
+        }else{
+            oMarker(cell);
+        }   
+    }
+});
