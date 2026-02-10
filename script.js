4a7f3ae345ee3f9a692aada69022247119039627
Shivam Wadhwa <shivamwadhwa7877@gmail.com>
2026-02-10 18:27:37 -0400

    Final update to this code. resetGame button is added to reset the game. Also moved the stuff to displayController and fixed some edge cases issue on UI.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const submitButton = document.querySelector("#startGame");
 submitButton.addEventListener("click", (event) => {
     const player1Name = document.querySelector("#player1NameInput").value;
     const player2Name = document.querySelector("#player2NameInput").value; 
-
+    displayController.resetBoard();
     gameController.startGame(player1Name, player2Name);
 });
 
@@ -51,20 +51,23 @@ const gameController = (function (){
         textBox.appendChild(content);
     }
 
+    const gameOverBox = document.querySelector("#gameOverBox");
+    const gameOverContent = document.createElement("h2");
+
     const resultBox = document.querySelector("#resultBox");
-    const content = document.createElement("h3");
+    const resultContent = document.createElement("h2"); 
     function playMove(row, column){
         if (gameOver){
-            content.innerText = "Game is already over."
-            resultBox.appendChild(content);
+            gameOverContent.innerText = "Game is already over."
+            gameOverBox.appendChild(gameOverContent);
 
             console.log("Game is already over.");
             return;
         }
 
         if (board[row][column] !== undefined){
-            content.innerText = "Invalid move: Cell Already Occupied";
-            resultBox.appendChild(content);
+            gameOverContent.innerText = "Invalid move: Cell Already Occupied";
+            gameOverBox.appendChild(gameOverContent);
 
             console.log("Invalid move: Cell Already Occupied");
             return;
@@ -76,8 +79,8 @@ const gameController = (function (){
         const result = checkWinner(row, column);
         if (result !== null){
             gameOver = true;
-            content.innerText = `${activePlayer.name} wins!`;
-            resultBox.appendChild(content);
+            resultContent.innerText = `${activePlayer.name} wins!`;
+            resultBox.appendChild(resultContent);
 
             console.log(`${activePlayer.name} wins!`);
             return placedMarker;
@@ -85,8 +88,8 @@ const gameController = (function (){
 
         if (checkTie()){
             gameOver = true;
-            content.innerText = "It's a tie!";
-            resultBox.appendChild(content);
+            resultContent.innerText = "It's a tie!";
+            resultBox.appendChild(resultContent);
 
             console.log("It's a tie!");
             return placedMarker;
@@ -154,29 +157,60 @@ const gameController = (function (){
     return {startGame, playMove, getActivePlayer};
 })();
 
-function xMarker(location) {
-    location.innerText = "X";
-    location.classList.add("cellDecoration");
-}
 
-function oMarker(location) {
-    location.innerText = "0";
-    location.classList.add("cellDecoration");
-}
-// Event Delegation
-document.addEventListener("click", (event) => {
-    if (event.target.matches(".cell")){
-        const cell = event.target;
-        const col = parseInt(cell.dataset.col);
-        const row = parseInt(cell.dataset.row);
-
-        const marker = gameController.playMove(row,col);
-
-        if (!marker) return;
-        if (marker === "X"){
-            xMarker(cell);
-        }else{
-            oMarker(cell);
-        }   
+const displayController = function () {
+    function xMarker(location) {
+        location.innerText = "X";
+        location.classList.add("cellDecoration");
     }
+
+    function oMarker(location) {
+        location.innerText = "0";
+        location.classList.add("cellDecoration");
+    }
+    // Event Delegation
+    document.addEventListener("click", (event) => {
+        if (event.target.matches(".cell")){
+            const cell = event.target;
+            const col = parseInt(cell.dataset.col);
+            const row = parseInt(cell.dataset.row);
+
+            const marker = gameController.playMove(row,col);
+
+            if (!marker) return;
+            if (marker === "X"){
+                xMarker(cell);
+            }else{
+                oMarker(cell);
+            }   
+        }
+    });
+
+    function resetBoard() {
+        const cells = document.querySelectorAll(".cell");
+
+        cells.forEach(cell => {
+            cell.innerText = "";
+            cell.classList.remove("cellDecoration");
+        });
+
+        const resultBox = document.querySelector("#resultBox");
+        resultBox.innerText = "";
+
+        const textBox = document.querySelector("#textBox");
+        textBox.innerText = "";
+
+        const gameOverBox = document.querySelector("gameOverBox");
+        gameOverBox.innerText = "";
+    }
+
+
+    return {resetBoard}
+}();
+
+const resetButton = document.querySelector(".resetGame");
+resetButton.addEventListener("click", () => {
+    gameController.startGame(player1NameInput.value, player2NameInput.value);
+    displayController.resetBoard();
 });
+
